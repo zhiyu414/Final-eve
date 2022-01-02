1ab471fe471084ef55255c8b42febc13ddbe838c
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2022-01-02 23:19:14 +0800

    first commit

---
+++ javascript/jquery.js
@@ -0,0 +1,81 @@
+$(function(){
+  $(".mvpic").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        vertical:false,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+      ]
+      });
+  
+});
+$('.mvpic').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+$(function() {
+  $('#spinner').fadeIn(2000);
+ 
+  // $('#spinner').fadeOut(5000);
+  $('#spinner').fadeOut(100);
+
+})
+move();
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+  }
+}
+$('.albumslider').slick({
+  
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+
+});
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  
+})
