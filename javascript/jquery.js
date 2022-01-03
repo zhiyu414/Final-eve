4bf62d014abff5b110368fe3fb94e79069c26049
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2022-01-03 23:37:42 +0800

    second

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -13,7 +13,7 @@ $(function(){
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 3,
             slidesToScroll: 1,
             infinite: true,
             arrows: false,
@@ -22,8 +22,8 @@ $(function(){
         {
           breakpoint: 768,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 1,
             arrows: false,
           }
         },
