6eb9ba73fdbbe20b304634c8e2528b363962e35d
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2022-01-03 17:46:52 +0800

    Add files via upload

---
+++ javascript/jquery.js
@@ -0,0 +1,105 @@
+$(function(){
+  $(".mvpic").slick({
+        
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        vertical:false,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 320,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+      ]
+      });
+  
+});
+
+
+
+
+
+
+
+
+
+$(function() {
+  $('#spinner').fadeIn(2000);
+ 
+  // $('#spinner').fadeOut(5000);
+  $('#spinner').fadeOut(100);
+
+})
+move();
+
+
+
+
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+  }
+}
+$('.albumslider').slick({
+  
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+
+});
+
+
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  
+})
+window.onscroll = scrollFunction; //每當畫面捲動觸發一次
+
+function scrollFunction() { 
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}//網頁捲動超過200pixel就會跑出來 display設定成block 跑回上面就隱藏。
+
+// 重置scrollTop這個變數的值
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
